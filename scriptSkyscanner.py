b3a223d0bf86c0166a11002a585922f12ab96a13
captainvinager <captainvinager@gmail.com>
2021-02-26 21:43:56 +0100

    Explotacion API

--- scriptSkyscanner.py
+++ scriptSkyscanner.py
@@ -1,14 +1,47 @@
 import requests
 
-url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/"
+def getJsonSkyScanner(origen, destino, fecha):
+    country = "ES"
+    currency = "EUR"
+    locale = "es-ES"
 
-querystring = {"query":"Stockholm"}
+    params = "/".join([country, currency, locale, origen, destino, fecha])
 
-headers = {
-    'x-rapidapi-key': "61982e9993msh5a7e13221d6bc82p183294jsn4ab0ad0313e1",
-    'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+    url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/" + params
+
+    querystring = {"inboundpartialdate": "2021-03-28"}
+
+    headers = {
+        'x-rapidapi-key': "61982e9993msh5a7e13221d6bc82p183294jsn4ab0ad0313e1",
+        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+    return requests.request("GET", url, headers=headers, params=querystring).json()
+def createTable(json):
 
-print(response.text)
+    precios = json['Quotes']
+    companias = json['Carriers']
+    lugares = json['Places']
+
+    for precio in precios:
+        total = precio['MinPrice']
+        for out in precio['OutboundLeg']['CarrierIds']:
+            for compania in companias:
+                if compania["CarrierId"] == out:
+                    companiaName = compania["Name"]
+            origen = [place for place in lugares if place['PlaceId'] == precio['OutboundLeg']['OriginId']][0]['Name']
+            destino = [place for place in lugares if place['PlaceId'] == precio['OutboundLeg']['DestinationId']][0][
+                'Name']
+        fecha = precio['OutboundLeg']['DepartureDate']
+
+        print(companiaName, origen, destino, fecha, total)
+
+
+origen = "MAD-sky"
+destino =["MVD-sky", "anywhere"]
+fecha = "2021-02-28"
+
+
+json = getJsonSkyScanner(origen, destino[0], fecha)
+
+print(json)
